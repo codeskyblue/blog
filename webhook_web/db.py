754ccdd514035d3b25d510b9fd6ff5a960eb38d7
codeskyblue <codeskyblue@gmail.com>
2023-05-04 20:47:22 +0800

    udpate db

--- webhook_web/db.py
+++ webhook_web/db.py
@@ -1,15 +1,51 @@
 # coding: utf-8
 
-from sqlalchemy import (
-    MetaData, Table, Column, ForeignKey,
-    Integer, String, Date, DateTime
-)
 import datetime
 
-meta = MetaData()
+from sqlalchemy import (Column, DateTime, ForeignKey, Integer, String,
+                        create_engine)
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
-history = Table('history', meta, 
-                Column('id', Integer, primary_key=True),
-                Column('gmt_create', DateTime, default=datetime.datetime.now),
-                Column('gmt_modified', DateTime, default=datetime.datetime.now),
-                Column('trigger', String, default='manual'))
+engine = create_engine('sqlite:///history.db', echo=True)
+Base = declarative_base()  #<-元类
+
+
+class History(Base):
+    __tablename__ = "auth_user"
+
+    id = Column(Integer, primary_key=True)
+    created_at = Column('created_at', DateTime, default=datetime.datetime.now)
+    updated_at = Column(DateTime,
+                        default=datetime.datetime.now,
+                        onupdate=datetime.datetime.now)
+    trigger = Column(String(30), default="manual")
+
+
+Base.metadata.create_all(engine)
+
+Session = sessionmaker(bind=engine)
+session = Session()
+
+# 创建一条记录
+new_record = History()
+# new_record.updated_at = datetime.datetime.now()
+session.add(new_record)
+session.commit()
+
+# 查询所有记录
+all_records = session.query(History).all()
+for record in all_records:
+    print(record.id, record.created_at, record.updated_at)
+
+# 更新一条记录
+record_to_update = session.query(History).first()
+# record_to_update.updated_at = datetime.datetime.now()
+session.commit()
+
+# 删除一条记录
+record_to_delete = session.query(History).first()
+session.delete(record_to_delete)
+session.commit()
+
+session.close()
